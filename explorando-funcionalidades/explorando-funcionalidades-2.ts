2f7413c141571f016bdcef883b7bc69ebc14a03c
Augustin Mesquita <augustomesquitasrs@gmail.com>
2018-05-13 22:35:32 -0300

    Adicionado testes para verificação de variáveis truthy ou falsy (que são tipos analisados a partir do contexto booleano do ecmascript).

--- explorando-funcionalidades/explorando-funcionalidades-2.ts
+++ explorando-funcionalidades/explorando-funcionalidades-2.ts
@@ -22,4 +22,48 @@ Object.keys(meuObjeto).forEach((key) => {
     }
 })
 
+// Bateria de testes que retorna a variável que está sendo verificada caso a mesma for truthy ou falsy.
+// O javascript considera algo truthy e ou falsy quando analisa o valor através do contexto Booleano.
+// Todos os valores são truthy a menos que eles sejam definidos como falsy (ou seja, exceto para false, 0, "", null, undefined, e NaN).
 
+let res;
+
+// Teste com array vazia.
+const testeUm = []; // Verdadeiro segundo contexto Booleano.
+res = testeUm || 'FALSO';
+imprimeResultado('testeUm', res)
+
+// Teste com string vazia.
+const testeDois = ""; // Falso segundo contexto Booleano.
+res = testeDois || 'FALSO'
+imprimeResultado('testeDois', res)
+
+// Teste com objeto vazio.
+const testeTres = {}; // Verdadeiro segundo contexto Booleano.
+res = testeTres || 'FALSO'
+imprimeResultado('testeTres', res)
+
+// Teste com zero.
+const testeQuatro = 0; // Falso segundo contexto Booleano.
+res = testeQuatro || 'FALSO'
+imprimeResultado('testeQuatro', res)
+
+// Teste com null.
+const testeCinco = null; // Falso segundo contexto Booleano.
+res = testeCinco || 'FALSO'
+imprimeResultado('testeCinco', res)
+
+const testeSeis = undefined; // Falso segundo contexto Booleano.
+res = testeSeis || 'FALSO'
+imprimeResultado('testeSeis', res)
+
+const testeSete = NaN; // Falso segundo contexto Booleano.
+res = testeSete || 'FALSO'
+imprimeResultado('testeSete', res)
+
+function imprimeResultado(nomeTeste, resultado: string) {
+    if (!(resultado == 'FALSO') {
+        resultado = 'VERDADEIRO';
+    }
+    console.log(`Resultado ${nomeTeste}: ${resultado}`)
+}
